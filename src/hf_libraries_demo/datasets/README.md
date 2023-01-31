13d703c1298f503fed88c6df58dff94ec677fa91
Brendan King <bking2@ucsc.edu>
2023-01-31 12:46:37 -0800

    adding example for loading a dataset

--- src/hf_libraries_demo/datasets/README.md
+++ src/hf_libraries_demo/datasets/README.md
@@ -4,3 +4,25 @@ I recommend reading through the [quickstart](https://huggingface.co/docs/dataset
 many of these examples come directly from this source.
 
 In this directory are examples of basic dataset operations, each grouped into a file or directory as appropriate.
+
+## Loading a Dataset
+
+For these examples, we'll use the classification dataset 
+[20-newsgroups](https://huggingface.co/datasets/SetFit/20_newsgroups)
+
+> The 20 newsgroups dataset comprises around 18000 newsgroups posts on 20 topics split in two subsets: one for training (or development) and the other one for testing (or for performance evaluation). The split between the train and test set is based upon a messages posted before and after a specific date.
+
+See the [Quickstart](https://huggingface.co/docs/datasets/quickstart) for more examples, as well as audio and vision datasets
+
+Example in [`load_dataset_example.py`](./load_dataset_example.py)
+
+### Useful Environment Variables for `nlp-gpu-01`
+
+See 
+Both `transformers` and `datasets` cache downloaded data and models, and can quickly overrun your home directory in 
+`/soe`. Add these environment variables to your run-time (customize to your user + location preference):
+
+```bash
+HF_HOME=/data/users/<username>/.cache/huggingface
+TRANSFORMERS_CACHE=/data/users/<username>/.cache/huggingface
+```
---
+++ src/hf_libraries_demo/datasets/load_dataset_example.py
@@ -0,0 +1,30 @@
+from pprint import pprint
+
+from datasets import load_dataset, DatasetDict, Dataset
+
+
+if __name__ == '__main__':
+    # loading the dataset will download if not present in your cache, or simply load from there
+    dataset: DatasetDict = load_dataset("SetFit/20_newsgroups")
+
+    # most datasets are loaded as DatasetDict's, where keys are splits. In this case we have 'train' and 'test'
+    assert sorted(list(dataset.keys())) == ['test', 'train'], f"unexpected splits or keys! {dataset}"
+    print(dataset)
+
+    # you can load a particular split by using the split argument, getting a Dataset instead of DatasetDict
+    train_dataset: Dataset = load_dataset("SetFit/20_newsgroups", split="train")
+
+    # you can iterate over a dataset with a for loop, or index as a list. Generally, you can treat datasets as a List
+    # of Dict objects which are loaded into memory only as needed, but also supporting map and filter operations
+    # unique to the datasets library
+    pprint(train_dataset[0])
+    for item in train_dataset:
+        # For this dataset, we have an input text of the newsgroup post ('text'), the label integer ('label') and
+        # the label as a string ('label_text')
+        assert 'text' in item and type(item['text']) == str
+        assert 'label_text' in item and type(item['label_text']) == str
+        assert 'label' in item and type(item['label']) == int
+
+    # loading separately is the same as just accessing from DatasetDict:
+    for item_a, item_b in zip(train_dataset, dataset['train']):
+        assert item_a == item_b, "datasets aren't the same!"
